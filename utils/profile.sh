75cc18104900fca6cf8b475ee4457fc5a216b7c6
Marco Neumann <marco@crepererum.net>
2013-04-01 18:32:49 +0200

    add profiling helper script

---
+++ utils/profile.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# config
+PFILE=test.profile
+
+# cleanup
+rm -f $PFILE
+rm -f *.db
+
+# run
+./cgrun.sh env LD_PRELOAD=/usr/lib/libprofiler.so.0 CPUPROFILE=$PFILE ./hugeDim &
+child=$!
+trap "kill -INT $child" INT
+wait $child
+
