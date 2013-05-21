9564eaeb41d7fe8f936464e0efe42b6deb47c2a7
Marco Neumann <marco@crepererum.net>
2013-05-21 14:56:45 +0200

    Better cgcreate script

--- utils/cgcreate.sh
+++ utils/cgcreate.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
-cat cg.conf | sed "s/_USER_/$USER/" | sudo tee -a /etc/cgconfig.conf
-sudo cgconfigparser -l /etc/cgconfig.conf
+# config
+target="/etc/cgconfig.conf"
+
+# parse template
+data=$(cat cg.conf | sed "s/_USER_/$USER/")
+
+# write config
+if [ -e "$target" ] && grep -q -F "$data" "$target"; then
+	echo "Skip existing config"
+else
+	echo "write config to $target"
+	echo "$data" | sudo tee -a "$target" 1>/dev/null
+fi
+
+# load config
+echo "Reload config"
+sudo cgconfigparser -l "$target" 1>/dev/null
 
